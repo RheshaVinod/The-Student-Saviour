41690f2ee6d57bfbe6a1017a8f91b6ad26968bb5
shr218 <63951538+shr218@users.noreply.github.com>
2021-02-27 21:41:08 +0530

    Add files via upload

--- h1.py
+++ h1.py
@@ -1,5 +1,5 @@
 from tkinter import*
-from stopwatch_final import *
+from stopwatch import *
 def todolist():
     
     inp_list=[]
@@ -10,7 +10,7 @@ def todolist():
         inp_list.append(i)
     print("End of list")
     def records():
-        f=open("record2.txt",'r')
+        f=open("record.txt",'r')
         s=f.readlines()
         for line in s:
             line=line.strip()
@@ -22,31 +22,19 @@ def todolist():
             for o in m:
                 print(o,end='   ')
             print()
-    def record1():
-        f=open("record1.txt",'r')
-        s=f.readlines()
-        for line in s:
-            line=line.strip()
-            line=line.split()
-        x=zip(inp_list,line)
-        t=tuple(x)
-        print("Time taken for each task:")
-        for m in t:
-            for o in m:
-                print(o,end='   ')
-            print()
-        
+    
         
 
     root=Tk()
     label2=Label(root,text="TO DO LIST",bg='black',fg='white')
     label2.pack()
-    ch1=Checkbutton(root,text='Day1',command = record1)
-    ch2=Checkbutton(root,text='Day1',command = record2)
+   # ch1=Checkbutton(root,text='Day1',command = records)
+   # ch2=Checkbutton(root,text='Day2',command = records1)
     for j in inp_list:
         bu1=Checkbutton(root,text=j,command = stopwatch)
         bu1.pack()
     lb=Button(root,text="Previous records",bg='purple',fg='white',command=records)
     lb.pack()
     root.mainloop()
+    print(z)
 todolist()
