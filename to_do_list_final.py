5ee40adf24ba54470b31d1d19574e646a39fcea3
morgan2002 <79154058+morgan2002@users.noreply.github.com>
2021-02-28 07:42:35 +0530

    Update to_do_list_final.py

--- to_do_list_final.py
+++ to_do_list_final.py
@@ -3,9 +3,9 @@ from tkinter import*
 from stopwatch_final import *
 def todolist():
     inp_list=[]
-    #n=int(input("Enter number of tasks to do today:"))
-    #print("Enter your to do's")
     root=Tk()
+    label2=Label(root,text="TO DO LIST",bg='black',fg='white')
+    label2.pack()
     inp_list=[]
     lb1=Label(root,text="Enter the number of tasks to do today")
     lb1.pack()
@@ -23,39 +23,18 @@ def todolist():
             
                 
                 n = e2.get()
+                
                 z="start stopwatch " 
                 bu1=Button(root,text=z,command = stopwatch)
                 bu1.pack()
     
-            #lb4 = Label(root, text = n)
-            #lb4.pack()
-            #inp_list.append(n)
             bt2 = Button(root, text = "Click Me2!", command = myClick2 ,bg = "blue",fg = "white")#callback
             bt2.pack()  
-       print("End of list")
     bt1 = Button(root, text = "Click Me!", command = myClick ,bg = "blue",fg = "white")#callback
     bt1.pack()
-     
     
-##    def records():
-##        f=open("record.txt",'r')
-##        s=f.readlines()
-##        for line in s:
-##            line=line.strip()
-##            line=line.split()
-##        x=zip(inp_list,line)
-##        t=tuple(x)
-##        print("Time taken for each task:")
-##        for m in t:
-##            for o in m:
-##                print(o,end='   ')
-##            print()   
-##        
-
-    label2=Label(root,text="TO DO LIST",bg='black',fg='white')
-    label2.pack()
-    #lb=Button(root,text="Previous records",bg='purple',fg='white',command=records)
-    #lb.pack()
     
     root.mainloop()
+    
 todolist()
+
