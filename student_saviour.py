816bede1aa6817af1887daacc98bde6c039dbdb7
Riya-654 <79152964+Riya-654@users.noreply.github.com>
2021-02-28 09:30:51 +0530

    Update student_saviour.py

--- student_saviour.py
+++ student_saviour.py
@@ -2,6 +2,7 @@ from tkinter import*
 import to_do_list_final as td
 import music_playlist as mp
 import website_blocker as wb
+import graph as g
 
 root=Tk()
 lb=Label(root,text="The Student Saviour")
@@ -11,9 +12,11 @@ lb2.grid(row=1,column=0)
 bt1=Button(root,text="Website Blocker",command=wb.webBlock)
 bt2=Button(root,text="To-Do-List",command=td.todolist)
 bt3=Button(root,text="Study Music",command=mp.music_playlist)
+bt4=Button(root,text="Statistical analysis",command=g.graph)
 bt1.grid()
 bt2.grid()
 bt3.grid()
+bt4.grid()
 root.mainloop()
 
 
