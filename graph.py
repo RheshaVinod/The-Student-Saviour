fa12d447542307dbd9f4f4fed67e87696f93f8bb
shr218 <63951538+shr218@users.noreply.github.com>
2021-02-28 09:31:07 +0530

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as pl
 from to_do_list_final import *
-l=[]
-for i in rang(1,message+1):
-  l.append('Task'+str(i))
+def graph() 
+  l=[]
+  for i in rang(1,message+1):
+    l.append('Task'+str(i))
 
-y=l
-pl.plot(x,y)
+  y=l
+  pl.plot(x,y)
